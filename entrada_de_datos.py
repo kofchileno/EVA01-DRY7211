2736ea9db3981401f5ea8552c365cdc076b7aec6
kofchileno <hector.valdebenitogandolfo@gmail.com>
2025-04-11 03:51:03 +0000

    SUBIR

---
+++ entrada_de_datos.py
@@ -0,0 +1,7 @@
+print("base de datos plana")
+name = input("Ingrese su nombre:")
+apellido = input("ingrese el apellido:")
+codig_sec = input("Ingresa tu codigo de Sección:")
+sede = input("Ingresa Sede Actual:")
+
+print("\n Nombre:"+ name,"\n Apellido:" + apellido,"\n Codigo_sec:"+codig_sec,"\n Sede:" + sede)
---
+++ integrantes.py
@@ -0,0 +1,2 @@
+print("Evaluacion N1 Programacion y Redes Virtualizadas\n")
+print("Integrantes:\n\n Héctor Valdebenito\n Matias Nelson \n")
